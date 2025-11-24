f5248fbd9426ad05bba4a39f9fd9f83b6d1cc4b6
domiquerendona <luisfelipecorreatj@gmail.com>
2025-11-23 19:38:42 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,14 @@
+def ensure_user(telegram_id: int, username: str = None):
+    user = get_user_by_telegram_id(telegram_id)
+    if user:
+        return user
 
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO users (telegram_id, username, role)
+        VALUES (?, ?, NULL);
+    """, (telegram_id, username))
+    conn.commit()
+    conn.close()
+    return get_user_by_telegram_id(telegram_id)
