4590d0292db4ceabcfab7f3025309d8ad8fdbec4
Luis Felipe Correa <luisfelipecorreatj@gmail.com>
2026-03-17 11:56:27 -0500

    fix: allow courier reset reauthorization after rejection

--- Backend/main.py
+++ Backend/main.py
@@ -315,7 +315,7 @@ def _append_registration_reset_button(keyboard, role_prefix: str, role_id: int, 
         keyboard.append([InlineKeyboardButton("Revocar reinicio", callback_data="{}_reset_clear_{}".format(role_prefix, role_id))])
         return
     if reset_state and reset_state.get("registration_reset_consumed_at"):
-        if role_prefix != "config_ally":
+        if role_prefix not in ("config_ally", "config_courier"):
             return
     keyboard.append([InlineKeyboardButton("Autorizar reinicio", callback_data="{}_reset_enable_{}".format(role_prefix, role_id))])
 
