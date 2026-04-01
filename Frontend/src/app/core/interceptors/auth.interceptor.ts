8da7693c43fd89e20f5359c18e72ed0e6fe0f149
LUIS ALFONSO MEJIA VELEZ <luisalfonsomejiavelez@MacBook-Air-de-LUIS.local>
2026-03-31 19:19:36 -0500

    Se volvio a poner bearer en el interceptor

--- Frontend/src/app/core/interceptors/auth.interceptor.ts
+++ Frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -7,7 +7,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     console.log('[interceptor] token:', token ? 'FOUND' : 'NULL');
     if (token) {
       req = req.clone({
-        setHeaders: { Authorization: token }
+        setHeaders: { Authorization: `Bearer ${token}` }
       });
     }
   } catch {
